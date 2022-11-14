108484f7a2684e052224b9695c2da799998e5ceb
Blas Piris <bpirisrdeveloper@gmail.com>
2022-11-14 10:46:21 +0100

    AlfaOmega_CRM_14-11-22 10:46

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,73 @@
+<section class="h-full gradient-form bg-gray-200 md:h-screen" style="
+background-color: #385052;
+background-image: url('https://www.transparenttextures.com/patterns/45-degree-fabric-light.png');
+">
+  <div class="container py-12 px-12 h-full" style="margin: 0 auto;">
+    <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+      <div class="xl:w-5/12">
+        <div class="block bg-white shadow-lg rounded-lg">
+          <div class="lg:flex lg:flex-wrap g-0">
+            <div class="lg:w-full px-4 md:px-0">
+              <div class="p-12 md:mx-6">
+                <div class="text-center">
+                  <img
+                    class="mx-auto w-48 rounded-lg"
+                    src="{{ asset('storage/images/logo.jpg') }}""
+                    alt="logo"
+                  />
+                  <h4 class="text-xl font-semibold mt-1 mb-6 pb-1">Alfa & Omega</h4>
+                </div>
+                <form>
+                  <p class="mb-4 text-center">Por favor, introduzca sus credenciales</p>
+                  <div class="mb-4">
+                    <input
+                      type="text"
+                      class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                      id="exampleFormControlInput1"
+                      placeholder="Usuario"
+                    />
+                  </div>
+                  <div class="mb-4">
+                    <input
+                      type="password"
+                      class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                      id="exampleFormControlInput1"
+                      placeholder="Contraseña"
+                    />
+                  </div>
+                  <div class="text-center pt-1 mb-12 pb-1">
+                    <button
+                      class="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
+                      type="button"
+                      data-mdb-ripple="true"
+                      data-mdb-ripple-color="dark"
+                      style="
+                        background: 
+                          #385052;
+            
+                      "
+                    >
+                     Acceder
+                    </button>
+                    <a class="text-gray-500" href="#!">Ha olvidado su contraseña?</a>
+                  </div>
+                  {{-- <div class="flex items-center justify-between pb-6">
+                    <p class="mb-0 mr-2">Don't have an account?</p>
+                    <button
+                      type="button"
+                      class="inline-block px-6 py-2 border-2 border-red-600 text-red-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                      data-mdb-ripple="true"
+                      data-mdb-ripple-color="light"
+                    >
+                      Danger
+                    </button>
+                  </div> --}}
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
